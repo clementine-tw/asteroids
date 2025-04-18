708bfc34ce0b0034d7ee229c52dbbba28b815151
clementine-tw <tsaiyouchen@gmail.com>
2025-04-18 21:13:04 +0800

    Draw a player that can be controlled by w,a,s,d on screen

--- main.py
+++ main.py
@@ -3,19 +3,35 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60)/1000
+
 
 # this line ensures the main()
 # function is only called when
